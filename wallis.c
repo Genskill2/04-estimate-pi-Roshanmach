b7f81023024481c9120a9bb6d387504b09833a17
Roshanmach <84711960+Roshanmach@users.noreply.github.com>
2021-06-02 20:26:12 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,10 +23,13 @@ int main(void) {
     }
   }
 }
-float wallis_pi(int x) 
-{
-	float pd=1.0;
-	for(int i=1; i<=x; i++) 
-  {p*= (4.0*i*i)/(float)((4.0*i*i)-1);}
-	return p*2;
+float wallis_pi(int a)
+ {	float pi;
+ 	float prd=1;
+ 	for(int i=1; i<=a; i++){
+ 	float num = (4.0*i*i)/((4.0*i*i)-1);
+ 	prd= prd*num;
+ 	}
+ 	pi= 2*prd;
+ 	return pi;
 }
